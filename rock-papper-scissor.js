ff2abdde543cad6afe86760a8c13b4fec1ff6a4b
Wassword <ahmedsaba271@gmail.com>
2024-06-14 21:07:38 -0400

    rps part 2

--- rock-papper-scissor.js
+++ rock-papper-scissor.js
@@ -3,17 +3,17 @@ const hands = ['rock' , 'paper', 'scissors'];
 //Define gethand function
  function getHand(){
    return hands[parseInt(Math.random()*10)%3]
-};
+}
 // Define 2 objects for 2 players
  const player1 ={
    name:'Player 1',
    getHand: getHand
-};
+}
 
- const player2 ={
+ const player2 = {
    name: "player 2",
    getHand: getHand
- };
+ }
 
 //Define playRound function
  function playRound(player1,player2){
@@ -39,14 +39,45 @@ const hands = ['rock' , 'paper', 'scissors'];
     } else {
        console.log(`${player2.name} wins!`)
        return player2
+      }
+      }
+     
+
+     function playGame(player1,player2, playUntil) {
+
+      player1.wins =0;
+      player2.wins =0;
+
+      function playRound() {
+        return Math.random() < 0.5 ? player1 : player2;
+       }
+     // play rounds until one player wins required amount of hands
+      while (player1.wins < playUntil && player2.wins < playUntil) { 
+         let winner= playRound();
+         winner.win += 1;
+         console.log(`${winner.name} wins this round. Total wins: ${winner.name}`);
+      }
+     
+     
+     let winnerPlayer = player1.wins >= playUntil ? player1 : player2;
+     console.log(`$winnerPlayer.name wins the game`);
+     return winnerPlayer;
+    
+     //Tournament function
+       function playTournament (player1,player2,player3,player4,playUntil) {
+
+        let g1Winner =playGame (player1,player2,playUntil);
+        let g2Winner =playGame (player3,player4,playUntil);
+        let worldChampion =playGame (g1Winner,g2Winner,playUntil);
+
+      console.log(`${worldChampion.name} is the tournament winner`);
     }
-    };
-     //Returns the winner object (null if no winner)
-      const winner= playRound(player1, player2);
-      if (winner) {
-      console.log(`The winner is ${winner.name'}in this round);
-                                   }else{ 
-       console.log('There is no ${winner.name`}in this round);
-     };
+  }; 
+  
+  let playUntil= 3;
+  playTournament(player1, player2, player3, player4, playUntil);
+
+     
+     
       
       
