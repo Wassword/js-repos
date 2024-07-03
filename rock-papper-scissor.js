930043580ddb9c859b310f34a4bfb96d394d070c
Wassword <ahmedsaba271@gmail.com>
2024-07-03 09:52:30 -0400

    Update rock-papper-scissor.js
    forgot to push

--- rock-papper-scissor.js
+++ rock-papper-scissor.js
@@ -6,13 +6,28 @@ const hands = ['rock' , 'paper', 'scissors'];
 }
 // Define 2 objects for 2 players
  const player1 ={
-   name:'Player 1',
+   name:'Sabah',
+   wins: 0,
    getHand: getHand
 }
 
  const player2 = {
-   name: "player 2",
+   name: "Gerald",
+   wins: 0,
    getHand: getHand
+
+   
+ }
+ const player3 ={
+  name:'Liah',
+  wins: 0,
+  getHand: getHand
+
+ }
+ const player4 ={
+  name:'XXO',
+  wins: 0,
+  getHand: getHand
  }
 
 //Define playRound function
@@ -21,17 +36,17 @@ const hands = ['rock' , 'paper', 'scissors'];
    let hand2 = player2.getHand();
 
    console.log(`${player1.name} played ${hand1}`)
-   console.log(`${player1.name} played ${hand1}`)
+   console.log(`${player2.name} played ${hand2}`)
    
 
    if (hand1 === hand2) {
-       console.log("Both players played ${hand1}. its a tie")
+       console.log(`Both players played ${hand1}. its a tie`)
        return null;
 
    } else if  (
        (hand1 === 'rock' && hand2 === "scissors") ||
        (hand1 === 'paper' && hand2 === "rock") ||
-       (hand1 === 'paper' && hand2 === 'scissors')
+       (hand1 === 'scissors' && hand2 === 'paper')
      //Determine the winner
     ) {
        console.log(`${player1.name} wins!`);
@@ -48,32 +63,41 @@ const hands = ['rock' , 'paper', 'scissors'];
       player1.wins =0;
       player2.wins =0;
 
-      function playRound() {
-        return Math.random() < 0.5 ? player1 : player2;
-       }
+    
      // play rounds until one player wins required amount of hands
       while (player1.wins < playUntil && player2.wins < playUntil) { 
-         let winner= playRound();
-         winner.win += 1;
-         console.log(`${winner.name} wins this round. Total wins: ${winner.name}`);
+         let winner= playRound(player1,player2);
+         if (winner != null) 
+            { if (player1.name === winner.name ) {
+            player1.wins += 1
+            
+         }   
+               
+         if (player2.name === winner.name ) {
+          player2.wins += 1
+
+          
+         }   console.log(`${winner.name} wins this round. Total wins: ${winner.wins}`);} 
+        
+      
       }
      
-     
-     let winnerPlayer = player1.wins >= playUntil ? player1 : player2;
-     console.log(`$winnerPlayer.name wins the game`);
+     let winnerPlayer = player1.wins > player2.wins ? player1 : player2;
+     console.log(`${winnerPlayer.name} wins the game`);
      return winnerPlayer;
-    
+    }; 
+
      //Tournament function
-       function playTournament (player1,player2,player3,player4,playUntil) {
+ function playTournament(player1,player2,player3,player4,playUntil) {
 
-        let g1Winner =playGame (player1,player2,playUntil);
-        let g2Winner =playGame (player3,player4,playUntil);
-        let worldChampion =playGame (g1Winner,g2Winner,playUntil);
+          let g1Winner = playGame(player1,player2,playUntil);
+          let g2Winner = playGame(player3,player4,playUntil);
+          let worldChampion = playGame(g1Winner, g2Winner, playUntil);
 
-      console.log(`${worldChampion.name} is the tournament winner`);
-    }
-  }; 
-  
+        console.log(`${worldChampion.name} is the tournament winner`);
+      }
+    
+    
   let playUntil= 3;
   playTournament(player1, player2, player3, player4, playUntil);
 
